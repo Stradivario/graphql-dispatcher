425bdbfafa184469cb87d86b119c59e08e621245
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-04-15 21:07:09 +0300

    feature(SingleExecutable): added whole dispatcher to be single executable

--- dist/app/core/send-request.d.ts
+++
@@ -1,8 +0,0 @@
-import { FetchResult } from 'apollo-link';
-export declare class RequestProvider {
-    sendRequest<T, V = {}>({ variables, query, token, }: {
-        variables?: V;
-        query: string;
-        token?: string;
-    }, uri: string): Promise<FetchResult<T>>;
-}
